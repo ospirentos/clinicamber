b452aceca6871631aef71128f678cd0abdc35a64
ospirentos <ospirentos@gmail.com>
2024-01-16 15:29:04 +0300

    fetch doctors in route with slug

--- amber-web-app/app/routes/doctors.$slug.jsx
+++ amber-web-app/app/routes/doctors.$slug.jsx
@@ -1,12 +1,33 @@
 import { useLoaderData } from "@remix-run/react";
+import i18next from "../i18next.server";
 
-export const loader = async ({ params }) => {
-  return params.slug;
+export const loader = async ({ params, request }) => {
+  let locale = await i18next.getLocale(request);
+  const apiUrl = process.env.API_URL;
+  const publicToken = process.env.PUBLIC_WEB_TOKEN;
+  let doctor = await fetch(
+    apiUrl +
+      "doctors?" +
+      new URLSearchParams({
+        locale: locale,
+        "populate[image][fields][0]": "url",
+        "filters[slug][$eq]": params.slug,
+      }),
+    {
+      method: "get",
+      headers: new Headers({
+        Authorization: "Bearer " + publicToken,
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
+    }
+  ).then((res) => res.json());
+  return { doctor };
 };
 
 export default function Doctor() {
-  const slug = useLoaderData();
-  console.log("slug", slug);
+  const { doctor } = useLoaderData();
+
+  console.log("Doctor is: ", doctor);
 
   return <div>Bu sayfa bilgisini içeriyor</div>;
 }
