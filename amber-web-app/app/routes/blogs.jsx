0e70cab2acae6470e2d876545a3ac5d7988cbf14
ospirentos <ospirentos@gmail.com>
2024-01-31 14:33:46 +0300

    feat: set max content width for blogs

--- amber-web-app/app/routes/blogs.jsx
+++ amber-web-app/app/routes/blogs.jsx
@@ -1,5 +1,11 @@
 import { Outlet } from "@remix-run/react";
 
 export default function Blogs() {
-  return <Outlet />;
+  return (
+    <div className="flex flex-1 items-center w-full h-auto justify-center">
+      <div className="px-6 w-full max-w-[1024px]">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
