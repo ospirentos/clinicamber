b5ee84c9ac180132987890cef22788763247cf68
ospirentos <ospirentos@gmail.com>
2024-02-20 03:01:57 +0300

    update api key to use it from .env file

--- amber-web-app/app/components/GoogleMap.jsx
+++ amber-web-app/app/components/GoogleMap.jsx
@@ -3,7 +3,7 @@ import { Loader } from "@googlemaps/js-api-loader";
 import { useEffect } from "react";
 
 const loader = new Loader({
-  apiKey: "AIzaSyABX3VA0Y-08bezPRrW3JsH7agjWB7yGvQ",
+  apiKey: process.env.WEB_GOOGLE_MAPS_API_KEY,
   version: "weekly",
   libraries: ["maps"],
 });
@@ -36,7 +36,7 @@ export const GoogleMap = () => {
               mark.addListener("click", () => {
                 window.open(
                   "https://www.google.com/maps/place/%C3%96ZEL+AMBER+A%C4%9EIZ+VE+D%C4%B0%C5%9E+SA%C4%9ELI%C4%9EI+POL%C4%B0KL%C4%B0N%C4%B0%C4%9E%C4%B0/@40.8679182,29.2804051,17z/data=!3m1!4b1!4m6!3m5!1s0x14cac6fd2c181e7d:0x36ff83eeacc6075e!8m2!3d40.8679142!4d29.28298!16s%2Fg%2F1tcy_k50?entry=ttu",
-                  "_blank",
+                  "_blank"
                 );
               });
             });
