11fd739acef28c51d9ab42be7d8b668663b5a6bd
ospirentos <ospirentos@gmail.com>
2024-11-02 19:24:31 +0300

    feat: Add translation to adress lines

--- amber-web-app/app/routes/_index.jsx
+++ amber-web-app/app/routes/_index.jsx
@@ -121,7 +121,7 @@ export default function MainPage() {
       <div className="relative text-center overflow-hidden max-w-[1080px] m-auto">
         <img
           src={banner}
-          alt="three pictures of clinic amber"
+          alt={t("bannerAltText", "three pictures of clinic amber")}
           className="w-[1080px]"
         />
         <div className="mx-auto sm:absolute top-[calc(50%-144px)] left-16 max-w-[400px]">
@@ -190,17 +190,17 @@ export default function MainPage() {
           <SectionTitle title={t("contact")} />
           <div className="flex gap-8 flex-wrap">
             <div className="w-full sm:w-1/2">
-              <h2 className="text-xl">Adres</h2>
+              <h2 className="text-xl">{t("addressTitle")}</h2>
               <p className="my-4">
-                Orhangazi, Esenyalı Orhangazi Mah, Alparslan Türkeş Cd No:2/A.
+                {t("addressLine1")}
                 <br />
                 <br />
-                Pendik/İstanbul
+                {t("addressLine2")}
               </p>
               <Map />
             </div>
             <div className="w-full sm:w-2/5 sm:flex-auto" id="contactUs">
-              <h2 className="text-xl">Bize Ulaşın</h2>
+              <h2 className="text-xl">{t("contactUsTitle")}</h2>
               <div className="flex justify-center flex-col"><ContactUsForm /></div>
             </div>
           </div>
@@ -208,7 +208,7 @@ export default function MainPage() {
         <div className="z-20 fixed right-4 bottom-6 lg:right-[calc(50%-512px)] sm:top-20 pointer-events-none">
           <a
             className="pointer-events-auto"
-            href="https://wa.me/905437622003?text=I'm%20interested%20in%20your%20car%20for%20sale"
+            href={t("whatsappLink", "https://wa.me/905437622003?text=I'm%20interested%20in%20your%20car%20for%20sale")}
           >
             <WhatsAppFloatingButton />
           </a>
