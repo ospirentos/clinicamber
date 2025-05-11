6b360001fcf417e94e1504017bd40abcf6595754
ospirentos <ospirentos@gmail.com>
2025-05-11 18:21:34 +0300

    fix: Remove prerender setup from build

--- amber-web-app/react-router.config.ts
+++ amber-web-app/react-router.config.ts
@@ -4,8 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  async prerender() {
-    return ["/kvkk"];
-  },
 } satisfies Config;
 
