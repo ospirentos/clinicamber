60b12298a53f35cb8959672b76bbacaf7037fe14
ospirentos <ospirentos@gmail.com>
2025-07-20 21:52:51 +0300

    refactor: simplify DentistCard and ServiceCard components by removing unused props and improving layout

--- amber-web-app/app/components/DentistCard.tsx
+++ amber-web-app/app/components/DentistCard.tsx
@@ -11,14 +11,14 @@ export function DentistCard({ name, role, image, slug }: Partial<DoctorAttribute
     <Link to={`/doctors/${slug}`} className="min-w-[8rem] my-4 sm:col-span-3 sm:h-[300px]">
       <Card
         radius="lg"
-        className="min-w-[12rem] my-4 sm:col-span-3 sm:h-[300px]"
+        className="min-w-[12rem] my-4 sm:col-span-3"
         isFooterBlurred
         isPressable
       >
         <Image
           alt="doctor image"
           className="object-cover"
-          src={ ENV.WEB_CMS_BASE_URL + image?.data.attributes.url}
+          src={ENV.WEB_CMS_BASE_URL + image?.data.attributes.url}
         />
         <CardFooter className="flex flex-col bg-black/10 shadow-small absolute bottom-0 z-10 p-1">
           <p className="text-tiny font-semibold text-white/80 mb-1">{name}</p>
--- amber-web-app/app/components/ServiceCard.tsx
+++ amber-web-app/app/components/ServiceCard.tsx
@@ -1,40 +1,31 @@
-import { Card, CardHeader, CardFooter } from "@heroui/card";
+import { Card, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { Link, useMatches } from "react-router";
 import type { ServiceAttributes } from "~/models/service.model";
 import type { RootLoader } from "~/root";
 
-export function ServiceCard({ title, shortText, image, slug }: Partial<ServiceAttributes>) {
+export function ServiceCard({ title, image, slug }: Partial<ServiceAttributes>) {
   const matches = useMatches();
   const { ENV } = matches.find((match) => match.id === "root")?.data as RootLoader;
 
   return (
-    <Link to={`/services/${slug}`} className="min-w-[12rem] my-4 col-span-12 sm:col-span-4 lg:col-span-3 sm:h-[300px]">
+    <Link to={`/services/${slug}`} className="min-w-[12rem] max-w-xs my-4 col-span-12 sm:col-span-4 lg:col-span-4">
       <Card
-        className="min-w-[12rem] my-4 sm:col-span-12 sm:h-[300px]"
+        className="min-w-[12rem] max-w-xs w-full h-[300px]"
         isFooterBlurred
         isPressable
       >
-        <CardHeader className=" z-10 top-1 flex-col ">
-          <h4 className="text-black font-medium text-large">{title}</h4>
+        <CardHeader className="z-10 flex-col">
+          <h5 className="text-black font-medium text-medium truncate w-full overflow-hidden whitespace-nowrap">{title}</h5>
         </CardHeader>
-        <Image
-          removeWrapper
-          alt="Card background"
-          className="z-0 w-full h-full object-cover"
-          src={ENV.WEB_CMS_BASE_URL + image?.data.attributes.url}
-        />
-        <CardFooter className="justify-center sm:justify-between bg-black/40 before:bg-white/10 border-white/20 overflow-hidden absolute bottom-0 w-full z-10">
-          <p className="hidden sm:block sm:text-tiny sm:text-white/80">
-            {shortText}
-          </p>
-          <div
-            className="text-tiny text-white bg-black/20"
-            color="default"
-          >
-            Daha Fazla
-          </div>
-        </CardFooter>
+        <div className="flex-1 overflow-hidden">
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="w-full h-full object-cover rounded-t-none"
+            src={ENV.WEB_CMS_BASE_URL + image?.data.attributes.url}
+          />
+        </div>
       </Card>
     </Link>
   );
